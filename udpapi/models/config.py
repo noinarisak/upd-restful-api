ca91abd55bd8e492bdf91bbe63325fced92c473f
noi narisak <noi.narisak@gmail.com>
2019-06-06 18:42:55 -0500

    Update model/config.py
    * Update Config schema to only have two attributes: udp_subdomain and
    context(JSON)

--- udpapi/models/config.py
+++ udpapi/models/config.py
@@ -1,3 +1,5 @@
+from sqlalchemy.dialects.postgresql import JSON
+
 from udpapi.extensions import db, pwd_context
 
 
@@ -6,12 +8,13 @@ class Config(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     udp_subdomain = db.Column(db.String(80), nullable=False)
-    demo_app_name = db.Column(db.String(255), nullable=False)
-    okta_api_key = db.Column(db.String(255), nullable=False)
-    okta_org_name = db.Column(db.String(255), nullable=False)
+    # demo_app_name = db.Column(db.String(255), nullable=False)
+    # okta_api_key = db.Column(db.String(255), nullable=False)
+    # okta_org_name = db.Column(db.String(255), nullable=False)
+    context = db.Column(JSON)
 
     def __init__(self, **kwargs):
         super(Config, self).__init__(**kwargs)
 
     def __repr__(self):
-        return "<Config %s>" % self.org_nametss
+        return "<Config %s>" % self.udp_subdomain
